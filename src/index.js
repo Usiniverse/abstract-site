a46cc5385403570dc86e67dac4bc5f9b09e780f6
Yushin Kang <rkddbtls007@naver.com>
2022-06-09 14:57:15 +0900

    테스트코드 작성

--- src/index.js
+++ src/index.js
@@ -1,10 +1,98 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        for (let i = 0; i < this.boards.length; i++) {
+            if (this.boards[i].name === board.name) {
+                throw new Error();
+            }
+        }
+        board.check = true;
+        this.boards.push(board);
+    }
 
-class Article {}
+    findBoardByName(boardName) {
+        return this.boards.find((board) => board.name === boardName);
+    }
+}
 
-class Comment {}
+class Board {
+    constructor(boardName) {
+        if (boardName === null) {
+            throw new Error();
+        } else if (boardName === '') {
+            throw new Error();
+        }
+        this.name = boardName;
+        this.check = false;
+        this.article = [];
+    }
+
+    publish(article) {
+        const date = new Date();
+        article.createdDate = date.toISOString();
+        article.id = `${this.name}-${Math.random()}`;
+
+        if (this.check === false) {
+            throw new Error();
+        }
+        article.check = true;
+        this.article.push(article);
+    }
+
+    getAllArticles() {
+        return this.article;
+    }
+}
+
+class Article {
+    constructor(article) {
+        if (article.subject === null || '') {
+            throw new Error();
+        } else if (article.content === null || '') {
+            throw new Error();
+        } else if (article.author === null || '') {
+            throw new Error();
+        }
+
+        this.subject = article.subject;
+        this.content = article.content;
+        this.author = article.author;
+        this.comment = [];
+        this.check = false;
+    }
+
+    reply(comment) {
+        const date = new Date();
+        comment.createdDate = date.toISOString();
+        comment.id = `${this.name}-${Math.random()}`;
+
+        if (this.check === false) {
+            throw new Error();
+        }
+
+        this.comment.push(comment);
+    }
+
+    getAllComments() {
+        return this.comment;
+    }
+}
+
+class Comment {
+    constructor(Comment) {
+        if (Comment.content === null || '') {
+            throw new Error();
+        } else if (Comment.author === null || '') {
+            throw new Error();
+        }
+
+        this.content = Comment.content;
+        this.author = Comment.author;
+    }
+}
 
 module.exports = {
     Site,
